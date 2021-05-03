fee51e24b449965a405804ab8626d7f555826272
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-05-03 17:08:02 +0600

    added contentful, retrieve recipe

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from "contentful";
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
+  return <div className="recipe-list">Recipe List</div>;
+}
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const response = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: response.items,
+    },
+  };
 }
