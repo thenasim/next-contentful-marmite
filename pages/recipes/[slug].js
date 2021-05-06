2032a23d5ebea04013c9bdbe189924853f8e3681
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-05-06 13:09:05 +0600

    get data to recipe page

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,43 @@
-export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
-    </div>
-  )
+import { createClient } from "contentful";
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
+  return <div>Recipe Details</div>;
+}
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export async function getStaticPaths() {
+  const response = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  const paths = response.items.map((item) => {
+    return {
+      params: {
+        slug: item.fields.slug,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      recipe: items[0],
+    },
+  };
 }
