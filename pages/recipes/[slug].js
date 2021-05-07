93aa9fe854bf777a3c122a156547698300dd8990
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-05-07 22:34:20 +0600

    rich text to react

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,8 +1,69 @@
+import Image from "next/image";
 import { createClient } from "contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export default function RecipeDetails({ recipe }) {
-  console.log(recipe);
-  return <div>Recipe Details</div>;
+  const {
+    featuredImage,
+    title,
+    cookingTime,
+    ingredients,
+    method,
+  } = recipe.fields;
+
+  return (
+    <div>
+      <div className="banner">
+        <Image
+          src={`https:${featuredImage.fields.file.url}`}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{title}</h2>
+      </div>
+
+      <div className="info">
+        <p>Takes about {cookingTime} minutes</p>
+        <h3>Ingredients:</h3>
+
+        {ingredients.map((ingredient, i) => (
+          <span key={i}>{ingredient}</span>
+        ))}
+      </div>
+
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+
+      <style jsx>{`
+        h2,
+        h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+      `}</style>
+    </div>
+  );
 }
 
 const client = createClient({
